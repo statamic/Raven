bb74476305131fc28460e2cb0b0eebd924989bb2
jackmcdade <jack@jackmcdade.com>
2014-02-12 15:12:59 -0500

    rename version to 1.1.1

--- add-ons/raven/pi.raven.php
+++ add-ons/raven/pi.raven.php
@@ -4,7 +4,7 @@ class Plugin_raven extends Plugin {
 
   public $meta = array(
     'name'       => 'Raven',
-    'version'    => '1.2',
+    'version'    => '1.1.1',
     'author'     => 'Statamic',
     'author_url' => 'http://statamic.com'
   );
