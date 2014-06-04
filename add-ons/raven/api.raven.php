6dbb23039e37ffbe106308ecfcb20717b6937ac3
Jack McDade <jack@jackmcdade.com>
2014-06-04 11:41:14 -0400

    basic metrics methods

--- add-ons/raven/api.raven.php
+++ add-ons/raven/api.raven.php
@@ -18,8 +18,9 @@ class API_raven extends API
 		$files   = $this->getFiles($formset_name);
 		$formset = $this->getFormset($formset_name);
 		$fields  = Helper::prettifyZeroIndexes(array_get($formset, 'control_panel:fields', $this->getFieldNames($files)));
+		$metrics = $this->buildMetrics(array_get($formset, 'control_panel:metrics'), $files);
 
-		return compact('files', 'fields', 'formset');
+		return compact('files', 'fields', 'formset', 'metrics');
 	}
 
 
@@ -102,5 +103,128 @@ class API_raven extends API
 		$formset['name'] = array_get($formset, 'name', $formset_name);
 
 		return $formset;
+	}
+
+	private function buildMetrics($operations, $data)
+	{
+		if ( ! is_array($operations)) {
+			return false;
+		}
+
+		$metrics = array();
+		foreach ($operations as $config) {
+			switch ($config['type']) {
+				case "unique":
+					$metrics[] = $this->metricUnique($config, $data);
+					break;
+				case "tally":
+					$metrics[] = $this->metricTally($config, $data);
+					break;
+				case "count":
+					$metrics[] = $this->metricCount($config, $data);
+					break;
+				case "average":
+					$metrics[] = $this->metricAverage($config, $data);
+					break;
+				case "median":
+					$metrics[] = $this->metricMedian($config, $data);
+					break;
+			}
+		}
+
+		return $metrics;
+	}
+
+	private function metricUnique($config, $data)
+	{
+    	$field = $config['field'];
+
+		$metrics = array();
+	    foreach ($data as $item) {
+	    	if (array_get($item['fields'], $field)) {
+				$metrics[] = $item['fields'][$field];
+		    }
+	    }
+
+	    $config['metrics'] = count(array_unique($metrics));
+
+		return $config;
+	}
+
+	private function metricTally($config, $data)
+	{
+		$field = $config['field'];
+
+		$metrics = array();
+	    foreach ($data as $item) {
+	    	$value = $item['fields'][$field];
+	        if (array_get($metrics, $value)) {
+	        	$metrics[$value] = $metrics[$value] +1;
+	        } else {
+	        	$metrics[$value] = 1;
+	        }
+	    }
+
+	    $config['metrics'] = $metrics;
+
+		return $config;
+	}
+
+	private function metricCount($config, $data)
+	{
+		$field = $config['field'];
+
+		$config['metrics'] = 0;
+	    foreach ($data as $item) {
+	    	if (array_get($item['fields'], $field)) {
+	    		$config['metrics']++;
+	    	}
+	    }
+
+		return $config;
+	}
+
+	private function metricAverage($config, $data)
+	{
+		$field = $config['field'];
+
+		$count = count($data);
+		$total = 0;
+	    foreach ($data as $item) {
+	    	$total = $total + array_get($item['fields'], $field);
+	    }
+
+	    $config['metrics'] = number_format(($total/$count), array_get($config, 'precision', 2));
+
+		return $config;
+	}
+
+	private function metricMedian($config, $data)
+	{
+		$field = $config['field'];
+
+		$count = count($data);
+
+		$numbers = array();
+	    foreach ($data as $item) {
+	    	$numbers[] = array_get($item['fields'], $field);
+	    }
+
+	    sort($numbers);
+	    $middle_value = floor(($count-1)/2);
+
+	    if ($count % 2) {
+	    	// odd number
+	    	$median = $numbers[$middle_value];
+	    } else {
+	    	// even number
+	    	$low = $number[$middle_value];
+	    	$high = $numbers[$middle_value + 1];
+	    	$median = (($low + $high)/2);
+	    }
+
+	    $config['metrics'] = $median;
+
+		return $config;
 	}
 }
