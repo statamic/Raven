af652e58eb47fa4bdc369758780e4db655622d2b
Jack McDade <jack@jackmcdade.com>
2014-11-18 11:22:11 -0500

    bump to 2.0.3

--- add-ons/raven/pi.raven.php
+++ add-ons/raven/pi.raven.php
@@ -4,7 +4,7 @@ class Plugin_raven extends Plugin {
 
 	public $meta = array(
 		'name'       => 'Raven',
-		'version'    => '2.0.2',
+		'version'    => '2.0.3',
 		'author'     => 'Statamic',
 		'author_url' => 'http://statamic.com'
 	);
