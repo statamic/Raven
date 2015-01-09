6cdf77fc520a0f5f758e49977e12e096243ea2a6
Jason Varga <jason@pixelfear.com>
2015-01-09 17:00:11 -0500

    Bump to 2.1

--- add-ons/raven/pi.raven.php
+++ add-ons/raven/pi.raven.php
@@ -4,7 +4,7 @@ class Plugin_raven extends Plugin {
 
 	public $meta = array(
 		'name'       => 'Raven',
-		'version'    => '2.0.4',
+		'version'    => '2.1.0',
 		'author'     => 'Statamic',
 		'author_url' => 'http://statamic.com'
 	);
